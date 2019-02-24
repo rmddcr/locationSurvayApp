5519dadff45d9bfc312d5539d024733b69e6bfba
Damitha <Ranasinghe>
2019-02-24 19:11:08 +0530

    added files

--- app/src/androidTest/java/com/jessicathornsby/tracking/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jessicathornsby/tracking/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.locationsurvayapp;
+package com.jessicathornsby.tracking;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.locationsurvayapp", appContext.getPackageName());
+        assertEquals("com.jessicathornsby.tracking", appContext.getPackageName());
     }
 }
